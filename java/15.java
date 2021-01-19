efd5e80ca83e85bfed1208300cb2606514c459d0
Milton <55441239+jmrchelani@users.noreply.github.com>
2021-01-19 21:54:12 +0500

    Create 15.java

---
+++ java/15.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static Node insert(Node head,int data) {
+        if(head == null) {
+            head = new Node(data);
+            return head;
+        }
+        for(Node i = head; i != null; i = i.next) {
+            if(i.next == null) {
+                Node temp = new Node(data);
+                i.next = temp;
+                return head;
+            }
+        }
+        return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
